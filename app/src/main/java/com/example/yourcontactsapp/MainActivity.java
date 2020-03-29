ed759d688a5004597f5a4e6844d4bae56d7fcbb2
Lucas Haney <lunehaney@hotmail.com>
2020-03-29 10:52:57 -0400

    Initial commit

---
+++ app/src/main/java/com/example/yourcontactsapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.yourcontactsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button1 = (Button) findViewById(R.id.btnViewContact1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Contact1.class));
+            }
+        });
+        Button button2 = (Button) findViewById(R.id.btnViewContact2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Contact2.class));
+            }
+        });
+    }
+}
